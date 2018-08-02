e84f44b4a53b197777434023bc58ab1081365f99
Loic DAVID <loic.david@altran.com>
2018-08-02 10:20:20 +0200

    Fixed references issues.

--- csharp-metar-display.tests/Properties/AssemblyInfo.cs
+++ csharp-metar-display.tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2383d9b-610c-46f6-987f-8ad06ded87ff")]
+[assembly: Guid("e447718b-2715-4760-818e-aeeca6a4d6f4")]
 
 // Version information for an assembly consists of the following four values:
 //
